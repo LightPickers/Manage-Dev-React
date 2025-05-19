5456ffe9ff070bb21cd9f0d500cb92907867b65c
zxlee0114 <napoleon.lee0114@gmail.com>
2025-05-20 01:41:10 +0800

    chore: 新增樣式

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import "@assets/all.scss";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import adminRouter from "@routes/router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={adminRouter} />
+    <ToastContainer position="bottom-center" autoClose={3000} />
   </React.StrictMode>
 );
