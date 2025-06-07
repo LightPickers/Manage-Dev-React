44d6bcd5a6ffd0691a97d6dfc28e9d2fdd285d7a
Hsiang <fdsa201305@gmail.com>
2025-06-07 23:21:41 +0800

    fix: 修改product相關schema檔名為productFormSchema，並放在products資料夾中

--- src/schemas/products/productFormSchema.js
+++ src/schemas/products/productFormSchema.js
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
-export const productCreateSchema = z.object({
+export const productFormSchema = z.object({
   primary_image: z.string({ required_error: "請上傳主圖" }).min(1, "請上傳主圖"),
   sub_images: z.array(z.string()).optional(),
   name: z.string().min(1, "商品名稱為必填"),
   category_id: z.string().min(1, "請選擇商品分類"),
   condition_id: z.string().min(1, "請選擇商品保存狀態"),
-  summary: z.array(z.string().min(1, "請填寫商品簡介")).min(1, "請至少填寫一項商品簡介"),
+  summary: z.array(z.string().min(1, "請填寫商品簡介")).min(1, "請至少填寫 1 項商品簡介"),
   title: z.string().min(1, "商品描述標題為必填"),
   subtitle: z.string().min(1, "商品描述副標題為必填"),
   description: z.any().refine(
@@ -29,28 +29,17 @@ export const productCreateSchema = z.object({
     invalid_type_error: "請選擇商品是否為精選",
   }),
   brand_id: z.string().min(1, "請選擇商品品牌"),
-  original_price: z.coerce
-    .number({ invalid_type_error: "市售價格式錯誤，請輸入數字" })
-    .min(1, "市售價必須大於 0"),
-  selling_price: z.coerce
-    .number({ invalid_type_error: "本網站價格式錯誤，請輸入數字" })
-    .min(1, "網站售價必須大於 0"),
-  hashtag1: z
-    .string()
-    .optional()
-    .refine(val => !val || val.startsWith("#"), {
-      message: "必須以 # 開頭",
-    }),
-  hashtag2: z
-    .string()
-    .optional()
-    .refine(val => !val || val.startsWith("#"), {
-      message: "必須以 # 開頭",
-    }),
-  hashtag3: z
-    .string()
-    .optional()
-    .refine(val => !val || val.startsWith("#"), {
-      message: "必須以 # 開頭",
-    }),
+  original_price: z.preprocess(
+    val => (val === "" ? undefined : Number(val)),
+    z
+      .number({ required_error: "請填寫市售價", invalid_type_error: "格式錯誤，請輸入數字" })
+      .positive("售價必須大於 0")
+  ),
+  selling_price: z.preprocess(
+    val => (val === "" ? undefined : Number(val)),
+    z
+      .number({ required_error: "請填寫網站售價", invalid_type_error: "格式錯誤，請輸入數字" })
+      .positive("售價必須大於 0")
+  ),
+  hashtag: z.array(z.string().min(1, "請填寫標籤")).min(1, "請至少填寫 1 項標籤"),
 });
